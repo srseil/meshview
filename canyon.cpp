b86e41e982cd380270c2cfdf36adf2a4be342ae0
Stefan Seil <stefan.seil@mailbox.org>
2025-10-29 14:33:10 +0100

    Rotating cube

--- canyon.cpp
+++ canyon.cpp
@@ -1,7 +1,68 @@
-#include <iostream>
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/common.hpp>
+#include <glm/ext.hpp>
+
+static const char* shaderCodeVertex = R"(
+    #version 460 core
+
+    layout (std140, binding = 0) uniform PerFrameData {
+        uniform mat4 mvp;
+        uniform int isWireframe;
+    };
+    layout (location = 0) out vec3 color;
+
+    const vec3 pos[8] = vec3[8](
+        vec3(-1.0, -1.0,  1.0),
+        vec3( 1.0, -1.0,  1.0),
+        vec3( 1.0,  1.0,  1.0),
+        vec3(-1.0,  1.0,  1.0),
+        vec3(-1.0, -1.0, -1.0),
+        vec3( 1.0, -1.0, -1.0),
+        vec3( 1.0,  1.0, -1.0),
+        vec3(-1.0,  1.0, -1.0)
+    );
+    const vec3 col[8] = vec3[8](
+        vec3(1.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        vec3(0.0, 0.0, 1.0),
+        vec3(1.0, 1.0, 0.0),
+        vec3(1.0, 1.0, 0.0),
+        vec3(0.0, 0.0, 1.0),
+        vec3(0.0, 1.0, 0.0),
+        vec3(1.0, 0.0, 0.0)
+    );
+    const int indices[36] = int[36](
+        0, 1, 2, 2, 3, 0,
+        1, 5, 6, 6, 2, 1,
+        7, 6, 5, 5, 4, 7,
+        4, 0, 3, 3, 7, 4,
+        4, 5, 1, 1, 0, 4,
+        3, 2, 6, 6, 7, 3
+    );
+
+    void main() {
+        int idx = indices[gl_VertexID];
+        gl_Position = mvp * vec4(pos[idx], 1.0);
+        color = isWireframe > 0 ? vec3(0.0) : col[idx];
+    }
+)";
+static const char* shaderCodeFragment = R"(
+    #version 460 core
+
+    layout (location = 0) in vec3 color;
+    layout (location = 0) out vec4 out_FragColor;
+
+    void main() {
+        out_FragColor = vec4(color, 1.0);
+    }
+)";
+
+struct PerFrameData {
+    glm::mat4 mvp;
+    int isWireframe;
+};
 
 int main()
 {
@@ -36,37 +97,6 @@ int main()
     gladLoadGL();
     glfwSwapInterval(1);
 
-    GLuint vao;
-    glCreateVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    static const char *shaderCodeVertex = R"(
-        #version 460 core
-        layout (location = 0) out vec3 color;
-        const vec2 pos[3] = vec2[3](
-            vec2(-0.6, -0.4),
-            vec2(0.6, -0.4),
-            vec2(0.0, 0.6)
-        );
-        const vec3 col[3] = vec3[3](
-            vec3(1.0, 0.0, 0.0),
-            vec3(0.0, 1.0, 0.0),
-            vec3(0.0, 0.0, 1.0)
-        );
-        void main() {
-            gl_Position = vec4(pos[gl_VertexID], 0.0, 1.0);
-            color = col[gl_VertexID];
-        }
-    )";
-    static const char* shaderCodeFragment = R"(
-        #version 460 core
-        layout (location = 0) in vec3 color;
-        layout (location = 0) out vec4 out_FragColor;
-        void main() {
-            out_FragColor = vec4(color, 1.0);
-        }
-    )";
-
     const GLuint shaderVertex = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(shaderVertex, 1, &shaderCodeVertex, nullptr);
     glCompileShader(shaderVertex);
@@ -81,14 +111,48 @@ int main()
     glLinkProgram(program);
     glUseProgram(program);
 
+    GLuint vao;
+    glCreateVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    const GLsizeiptr kBufferSize = sizeof(PerFrameData);
+    GLuint perFrameDataBuf;
+    glCreateBuffers(1, &perFrameDataBuf);
+    glNamedBufferStorage(perFrameDataBuf, kBufferSize, nullptr, GL_DYNAMIC_STORAGE_BIT);
+    glBindBufferRange(GL_UNIFORM_BUFFER, 0, perFrameDataBuf, 0, kBufferSize);
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_POLYGON_OFFSET_LINE);
+    glPolygonOffset(-1.0f, -1.0f);
+
     while (!glfwWindowShouldClose(window)) {
         int width, height;
         glfwGetFramebufferSize(window, &width, &height);
         glViewport(0, 0, width, height);
 
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        const float ratio = width / (float)height;
+        const glm::mat4 model = glm::rotate(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.5f)), (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
+        const glm::mat4 perspective = glm::perspective(45.0f, ratio, 0.1f, 1000.0f);
+
+        // Render filled cube
+        PerFrameData perFrameData = {
+            perspective * model,
+            false
+        };
+        glNamedBufferSubData(perFrameDataBuf, 0, kBufferSize, &perFrameData);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // Render wireframe cube
+        perFrameData.isWireframe = true;
+        glNamedBufferSubData(perFrameDataBuf, 0, kBufferSize, &perFrameData);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
